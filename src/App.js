3eb3e31d3a030266a0d816fb642ae9f2445b6785
Sergiu Marcu <sergiu.marcu@gremini.com>
2023-02-06 16:55:59 +0200

    Basic form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+
+  let [name, setName] = useState('')
+  let [password, setPassword] = useState('')
+
+  let handleInput = (func, value) => {
+    func(value);
+  }
+  
+
+  let handleFormSubmit = (e) => {
+    e.preventDefault();
+    alert('Succes!')
+    console.log(e);
+    console.log(`Name: ${name}; Password:${password}`)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form" onSubmit={(e) => handleFormSubmit(e)}>
+        <div>
+        <label>Nume: </label>
+          <input type="text" name="name" value={name} onChange={(e) => handleInput(setName, e.target.value)}></input>
+        </div>
+        
+        <div>
+        <label>Parola: </label>
+          <input type="password" name="password" value={password} onChange={(e) => handleInput(setPassword, e.target.value)}></input>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+      
     </div>
   );
 }
